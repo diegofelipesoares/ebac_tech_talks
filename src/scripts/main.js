a98f55abc99da9bce30d364f24eed97a7255d85e
Diego Soares <diegofelipesoares@gmail.com>
2025-04-28 18:29:42 -0300

    Alterando configurações

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,7 @@ AOS.init();
 
 //Criando data no futuro. 
 //"mes dia, ano, hora"
-const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const dataDoEvento = new Date("Dec 12, 2030 19:00:00");
 
 //Vamos agora recuperar o dataStamp.
 const timeStampDoEvento = dataDoEvento.getTime();
